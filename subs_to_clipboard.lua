6d7ba152036326cc8ae5ae03ce13ef8c3c821fef
Francis <Francis@Dell-XPS.local>
2019-12-16 20:08:08 -0500

    Add copy to clipboard support for windows

--- subs_to_clipboard.lua
+++ subs_to_clipboard.lua
@@ -1,9 +1,26 @@
 --- ___Usage___:
---- toggle-copy-subs: Automatically copy subtitles to clipboard as they appear. Works on mac 10.13
+--- toggle-copy-subs: Automatically copy subtitles to clipboard as they appear. Works on mac 10.13 and windows 10
 ---                    input.conf example:
 ---                     * script-message-to subs_to_clipboard toggle-subs-to-clipboard
 
 _G.autocopysubs = false
+
+--- platform detection taken from: github.com/rossy/mpv-repl/blob/master/repl.lua
+function detect_platform()
+    local o = {}
+    if mp.get_property_native('options/vo-mmcss-profile', o) ~= o then
+        return 'windows'
+    elseif mp.get_property_native('options/cocoa-force-dedicated-gpu', o) ~= o then
+        return 'macos'
+    end
+    return 'linux'
+end
+
+_G.platform = detect_platform()
+if _G.platform == 'windows' then
+    _G.utils = require 'mp.utils'
+end
+--- end platform detection code
 
 function escape(s)
   return (s:gsub('\'', '\'\\\'\''))
@@ -11,10 +28,24 @@ end
 
 function copy_sub(prop, subtext)
     if subtext and subtext ~= '' then
-        os.execute("export LANG=en_US.UTF-8; echo '" .. escape(subtext) .. "' | pbcopy")
+        if _G.platform == 'macos' then
+            os.execute("export LANG=en_US.UTF-8; echo '" .. escape(subtext) .. "' | pbcopy")
+        elseif _G.platform == 'windows' then
+            --windows copy taken from hsyong, github.com/mpv-player/mpv/issues/4695
+            local escapedtext = string.gsub(mp.get_property("sub-text"), "'", "")
+            local res = _G.utils.subprocess({ args = {
+                'powershell', '-NoProfile', '-Command', string.format([[& {
+                    Trap {
+                        Write-Error -ErrorRecord $_
+                        Exit 1
+                    }
+                    Add-Type -AssemblyName PresentationCore
+                    [System.Windows.Clipboard]::SetText('%s')
+                }]], escapedtext)
+            } })
+        end
     end
 end
-
 
 function stop_auto_copy_subs()
     mp.osd_message("Auto-copy subs disabled", 1)
