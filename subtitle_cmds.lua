ee3fa32ea8f57337f32e6c4b14d5bc8e31a1c612
perogies <MY_NAME@example.com>
2019-12-16 20:08:08 -0500

    Add copy to clipboard support for windows

--- subtitle_cmds.lua
+++ subtitle_cmds.lua
@@ -1,6 +1,6 @@
 --- ___Usage___:
 --- ab-loop-sub [autopause]: Set ab-loop to current subtitle. Specify 'autopause' arg to pause before every loop
---- copy-subtitle:           Copy current subtitle to clipboard. Works on mac 10.13
+--- copy-subtitle:           Copy current subtitle to clipboard. Works on mac 10.13 and windows 10
 --- replay-subtitle:         Replay current or previous subtitle.
 --- step-subtitle:           End current subtitle or start next subtitle.
 ---
@@ -11,6 +11,23 @@
 ---  KP2 script-message-to subtitle_cmds copy-subtitle
 
 _G.abloopavoidjankpause = false
+
+--- platform detection taken from: github.com/rossy/mpv-repl/blob/master/repl.lua
+function detect_platform()
+    local o = {}
+    if mp.get_property_native('options/vo-mmcss-profile', o) ~= o then
+        return 'windows'
+    elseif mp.get_property_native('options/cocoa-force-dedicated-gpu', o) ~= o then
+        return 'macos'
+    end
+    return 'linux'
+end
+
+_G.platform = detect_platform()
+if _G.platform == 'windows' then
+    _G.utils = require 'mp.utils'
+end
+--- end platform detection code
 
 function pause_on_sub_loop(prop, value)
     if _G.abloopavoidjankpause == true then
@@ -63,7 +80,22 @@ end
 function copy_subtitle()
     local subtext = mp.get_property("sub-text")
     if subtext and subtext ~= '' then
-        os.execute("export LANG=en_US.UTF-8; echo '" .. escape(subtext) .. "' | pbcopy")
+        if _G.platform == 'macos' then
+            os.execute("export LANG=en_US.UTF-8; echo '" .. escape(subtext) .. "' | pbcopy")
+        elseif _G.platform == 'windows' then
+            --windows copy taken from hsyong, github.com/mpv-player/mpv/issues/4695
+            local escapedtext = string.gsub(mp.get_property("sub-text"), "'", "")
+            local res = _G.utils.subprocess({ args = {
+                'powershell', '-NoProfile', '-Command', string.format([[& {
+                    Trap {
+                        Write-Error -ErrorRecord $_
+                        Exit 1
+                    }
+                    Add-Type -AssemblyName PresentationCore
+                    [System.Windows.Clipboard]::SetText('%s')
+                }]], escapedtext)
+            } })
+        end
         mp.osd_message(subtext, 0.5)
     else
         mp.osd_message("No subtitles present", 0.5)
